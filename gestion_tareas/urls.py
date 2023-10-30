9fc18842457d4293da16956d106c945785289f3f
Elvis Zhune <elfabri28@gmail.com>
2023-10-30 01:05:04 +0100

    Ejercicio 7 terminado

--- gestion_tareas/urls.py
+++ gestion_tareas/urls.py
@@ -6,9 +6,17 @@ urlpatterns = [
     
     path('proyecto/listar', views.proyecto_list, name='proyecto_list'),
     
-    path('proyecto/<int:proyecto_id/tareas', views.proyecto_tareas_list, name='tarea_list'),
+    path('proyecto/<int:proyecto_id>/tareas', views.proyecto_tareas_list, name='tarea_list'),
     
     path('tarea/<int:tarea_id>/usuarios', views.usuario_asignado_tarea_list, name='usuario_list'),
+    
+    path('tareas/<str:texto_tarea>',views.tareas_texto_concreto, name='tarea_list'),
+    
+    path('proyectos/completados-entre-fechas/<str:fecha_inicio>/<str:fecha_fin>', views.proyectos_completados_entre_fechas, name='proyectos_completados_entre_fechas'),
+    
+    path('proyecto/<int:proyecto_id>/ultimo-usuario-comentario',views.ultimo_usuario_comentario_tarea_proyecto, name='ultimo_usuario_comentario_tarea_proyecto'),
+    
+    
     
     path('etiqueta', views.etiqueta_list, name='etiqueta_list'),
     
--- gestion_tareas/views.py
+++ gestion_tareas/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Usuario, Tarea, Proyecto, Etiqueta, Asignacion_Tarea, Comentario
+from datetime import datetime
 
 # Create your views here.
 def index(request):
@@ -19,6 +20,35 @@ def usuario_asignado_tarea_list(request, tarea_id):
     asignaciones = Asignacion_Tarea.objects.filter(tarea__id=tarea_id).order_by('fecha_asignacion')
     usuarios_asignados = [asignacion.usuario for asignacion in asignaciones]
     return render(request, 'usuario/usuario_list.html', {'usuarios_mostrar':usuarios_asignados})
+
+def tareas_texto_concreto(request, texto_tarea):
+    tareas = Tarea.objects.select_related("creador", "proyecto").prefetch_related("usuarios_asignados")
+    tareas = tareas.filter(asignacion_tarea__observaciones__contains=texto_tarea)
+    return render(request, 'tarea/tarea_list.html',{"tareas_mostrar":tareas})
+    
+def proyectos_completados_entre_fechas(request, fecha_inicio, fecha_fin):
+    fecha_inicio = datetime.strptime(fecha_inicio, '%Y-%m-%d')
+    fecha_fin = datetime.strptime(fecha_fin, '%Y-%m-%d')
+    proyectos = Proyecto.objects.select_related("creador_proyecto").prefetch_related("proyectos_asignados")
+    proyectos = proyectos.filter(
+        fecha_inicio__gte=fecha_inicio,
+        fecha_finalizacion__lte=fecha_fin,
+    )
+    return render(request, 'proyecto/proyecto_list.html', {'proyectos_mostrar':proyectos})
+
+def ultimo_usuario_comentario_tarea_proyecto(request, proyecto_id):
+    ultimo_comentario = Comentario.objects.select_related("autor", "tarea")
+    ultimo_comentario = ultimo_comentario.filter(tarea__proyecto=proyecto_id).order_by("-fecha_comentario").first()
+    if ultimo_comentario:
+        ultimo_usuario_comentario = ultimo_comentario.autor
+    else:
+        ultimo_usuario_comentario=None
+        
+    return render(request, "comentario/comentario_list.html",{"ultimo_usuario_comentario":ultimo_usuario_comentario})
+    
+    
+    
+
 
 def etiqueta_list(request):
     etiquetas = Etiqueta.objects.all()
